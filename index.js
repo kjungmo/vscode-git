d51de48ac26d2cfb2463f7d7b813a88b89cbbb41
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-02 22:56:01 +0900

    Event 1

---
+++ index.js
@@ -0,0 +1,4 @@
+const title = document.getElementById("title"); //html에서 가져오는 방법이다
+
+console.log(title);
+//Document Object Module 이것을 통해 자바스크립트는 html에 있는 모든 요소를 가져온다. 그리고 이것을 객체로 바꿀 것이다. html태그를 가져다가 객체로 만든다. 자바스크립트로 html을 바꿀 수 있다. 
