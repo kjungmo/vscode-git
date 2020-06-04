e02cba822bc926367368091acbeb5a1cf987b20e
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-04 17:52:14 +0900

    classlist

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ const CLICKED_CLASS = "clicked";
 function handleClick() {
     const currentClass = title.className;
     if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS;
+        title.classList.add(CLICKED_CLASS);
     } else {
-        title.className = "";
+        title.classList.remove(CLICKED_CLASS);
     }
 }
 
