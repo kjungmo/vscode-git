45f33c4a70996af0b17053b96c2c8f8d404cc87c
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-04 17:50:26 +0900

    making events

--- index.js
+++ index.js
@@ -59,8 +59,19 @@ function handleClick() {
     }
 }
 */
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    } else {
+        title.className = "";
+    }
+}
+
 function init() {
     //title.style.color = BASE_COLOR;
-    title.addEventListener("mouseenter", handleClick);
+    title.addEventListener("click", handleClick);
 }
 init();
