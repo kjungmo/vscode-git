d7f4cbcb2e4423921a6fab95328ddb3d972a6c0d
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-05 21:25:17 +0900

    + seconds (but still not working)

--- clock.js
+++ clock.js
@@ -1,11 +1,12 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");l
+  clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
-    clockTitle.innerText = `${hours}:${minutes}`;
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function init() {
