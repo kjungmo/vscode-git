fc1ef43494eac7ee4b235c03b61d67726db114a8
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-05 21:19:37 +0900

    making JS clock pt1

---
+++ clock.js
@@ -0,0 +1,11 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");l
+
+function getTime() {
+    const date = new Date();
+}
+
+function init() {
+
+}
+init();
