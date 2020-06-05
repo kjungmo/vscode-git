0853b8bad2187551efa6f54be09825196717b3be
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-05 21:58:16 +0900

    향상된 for문 비슷한 문법으로 (작은if라고 불렀다) 그걸로 시간 변화 하는거에 조건줬다

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 const clockContainer = document.querySelector(".js-clock"),
- clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
@@ -7,7 +7,9 @@ function getTime() {
     const hours = date.getHours();
     const seconds = date.getSeconds();
 
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 9 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
