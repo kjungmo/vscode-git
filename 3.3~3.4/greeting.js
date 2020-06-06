cc1ae1fd5ba0025c488433267c3013795e646689
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-06 22:50:28 +0900

    유저가 있는경우와 없는경우 css, html, js 코드

--- 3.3~3.4/greeting.js
+++ 3.3~3.4/greeting.js
@@ -1,15 +1,20 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
 
 const USER_LS = "currentUser"
 
+function paintGretting(text){
+    greeting.innerText = `tello ${text}`
+}
+
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
+    if(currentUser === null){  // 유저가 없는 경우
 
-    } else {
-        
+    } else {   // 유저가 있는 경우
+
     }
 }
 
