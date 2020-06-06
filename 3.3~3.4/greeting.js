c668921ad6ec7f097f0b7ad07bc751285a15d8fc
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-06 22:14:17 +0900

    using localStorage

--- 3.3~3.4/greeting.js
+++ 3.3~3.4/greeting.js
@@ -1,5 +1,20 @@
-function init(){
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input");
 
+
+const USER_LS = "currentUser"
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+
+    } else {
+        
+    }
+}
+
+function init(){
+    loadName();
 }
 
 init();
