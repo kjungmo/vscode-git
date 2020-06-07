3ecc34c5e8a7d439fe8d9dd31281b11f3c8a59c1
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-07 22:29:41 +0900

    saved

---
+++ 3.3~3.4/6quiz/index.js
@@ -0,0 +1,25 @@
+
+
+var nara = document.getElementById("userCountry");
+var User = nara.options[nara.selectedIndex].value;
+
+
+
+/*
+const select = document.querySelector(".contries");
+
+function saveContry() {
+  const selectCountry = "contry";
+  localStorage.setItem(selectCountry, select.value);
+}
+
+function handleChange() {
+  select.addEventListener("change", saveContry);
+}
+
+function init() {
+  handleChange();
+}
+init();
+
+*/
