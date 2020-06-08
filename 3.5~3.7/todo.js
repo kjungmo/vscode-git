7b2c093f32ee1950135d735e3ecebe070bf4c593
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-08 15:35:20 +0900

    toDos array에 어떻게 값을 넣을 것인가

--- 3.5~3.7/todo.js
+++ 3.5~3.7/todo.js
@@ -4,7 +4,7 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+const toDos = []; //toDo가 여러개 생길 수 있기 때문에 array사용. 시작에 이렇게 해두자. 그리고나서는 toDo가 생성되면 array에 추가되도록 한다.
 
 function paintToDo(text){
     const li = document.createElement("li"); //js에서 html 수정 않고도 li라는 것을 생성해주는 작업이다. 괄호안의 li 를 입력하는게 중요 
@@ -17,6 +17,11 @@ function paintToDo(text){
     toDoList.appendChild(li); //그리고는 만든 li를 넣고 appendchild todolist 해줌
  //이 결과로 엔터를 눌렀을 때, li를 생성하고, delete버튼과  span을 생성한다
  // span과 delete버튼을 li안에 append하고, 마지막으로 li를 ul에다 append 하게 된다.
+    const toDoObj = {
+        text: text,
+        id: toDos.length + 1 //맨 처음엔 이 toDos array가 비어있으니까 id 값은 1 일것임.
+    };
+    toDos.push(toDoObj); //이렇게 push를 써서 array안에 element 하나를 넣어줄 수 있다. 
 }
 
 function handleSubmit(event){ // event 넣어줘서 prevent 할거다
